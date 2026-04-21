dcecf2f77e48b1d968aee16740ebbc0936e7dc86
Sonia-txt <Soniaitzvaz@gmail.com>
2026-04-21 15:10:58 -0600

    creacion de modelos

--- Store.Api/Models/SaleDetail.cs
+++ Store.Api/Models/SaleDetail.cs
@@ -2,5 +2,10 @@ namespace Store.Api.Models;
 
 public class SaleDetail
 {
-    
+    public int Id { get; set; }
+    public int SaleId { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public decimal Subtotal { get; set; }
 }
