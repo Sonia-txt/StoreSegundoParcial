23ca7591774b4370f60387ae91550208bb92ab1c
maribel798-txt <lrs22110150@purisima.tecnm.mx>
2026-04-21 17:45:31 -0600

    crear metodos de sales y users

--- Store.Api/Repositories/Interface/ISaleRepository.cs
+++ Store.Api/Repositories/Interface/ISaleRepository.cs
@@ -1,6 +1,15 @@
 namespace Store.Api.Repositories.Interface;
-
+using Store.Core.Entities;
 public interface ISaleRepository
 {
-    
+    //metodo para guardar las categorias del producto
+    Task<Sale> SaveAsync(Sale sale);
+    //metodo para actualizar las categorias del producto
+    Task<Sale> UpdateAsync(Sale sale);
+    //metodo para retornat una lidta de categorias de producto
+    Task<List<Sale>> GetAllAsync();
+    //metodo para retornar el id de las categorias del producto que se borrara
+    Task<bool> DeleteAsync(int id);
+    //metodo para obtener una categoria por id
+    Task<Sale> GetById(int id);
 }
--- Store.Api/Repositories/Interface/IUserRepository.cs
+++ Store.Api/Repositories/Interface/IUserRepository.cs
@@ -1,6 +1,15 @@
 namespace Store.Api.Repositories.Interface;
-
+using Store.Core.Entities;
 public interface IUserRepository
 {
-    
+    //metodo para guardar las categorias del producto
+    Task<User> SaveAsync(User user);
+    //metodo para actualizar las categorias del producto
+    Task<User> UpdateAsync(User user);
+    //metodo para retornat una lidta de categorias de producto
+    Task<List<User>> GetAllAsync();
+    //metodo para retornar el id de las categorias del producto que se borrara
+    Task<bool> DeleteAsync(int id);
+    //metodo para obtener una categoria por id
+    Task<User> GetById(int id);
 }
