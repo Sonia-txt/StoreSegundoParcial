43ac3c4808915da906b8757fc71536262ef240b5
maribel798-txt <lrs22110150@purisima.tecnm.mx>
2026-04-21 17:43:25 -0600

    crear metodos de customer

--- Store.Api/Repositories/Interface/ICustomerRepository.cs
+++ Store.Api/Repositories/Interface/ICustomerRepository.cs
@@ -1,6 +1,15 @@
 namespace Store.Api.Repositories.Interface;
-
+using Store.Core.Entities;
 public interface ICustomerRepository
 {
-    
+    //metodo para guardar las categorias del producto
+    Task<Customer> SaveAsync(Customer customer);
+    //metodo para actualizar las categorias del producto
+    Task<Customer> UpdateAsync(Customer customer);
+    //metodo para retornat una lidta de categorias de producto
+    Task<List<Customer>> GetAllAsync();
+    //metodo para retornar el id de las categorias del producto que se borrara
+    Task<bool> DeleteAsync(int id);
+    //metodo para obtener una categoria por id
+    Task<Customer> GetById(int id);
 }
