73cef31c602af098bfe83b943276633633c313ae
maribel798-txt <lrs22110150@purisima.tecnm.mx>
2026-04-21 15:59:30 -0600

    Crear mas DataAccess

--- Store.Api/DataAccess/DbContext.cs
+++ Store.Api/DataAccess/DbContext.cs
@@ -1,26 +1,6 @@
-using System.Data.Common;
-using MySqlConnector;
-using Store.Api.DataAccess.Interfaces;
 namespace Store.Api.DataAccess;
 
-
-public class DbContext : IDbContext
+public class DbContext
 {
-    private readonly string _connectionString=
-        "server=localhost;user=root;password=1234;database=;port=3306";
-    private MySqlConnection _connection;
-
-    public DbConnection Connection
-    {
-        get
-        {
-            if (_connection == null)
-            {
-                _connection = new MySqlConnection(_connectionString);
-
-            }
-
-            return _connection;
-        }
-    }
+    
 }
--- Store.Api/DataAccess/Interfaces/IDbContext.cs
+++ Store.Api/DataAccess/Interfaces/IDbContext.cs
@@ -1,7 +1,7 @@
 namespace Store.Api.DataAccess.Interfaces;
-using System.Data.Common;
+
 
 public interface IDbContext
 {
-    DbConnection Connection { get; }
+    
 }
