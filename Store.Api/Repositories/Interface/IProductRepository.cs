b879ffb0540d79dc966a7b14a07cea581d45ddf1
maribel798-txt <lrs22110150@purisima.tecnm.mx>
2026-04-21 17:43:55 -0600

    crear metodos de products

--- Store.Api/Repositories/Interface/IProductRepository.cs
+++ Store.Api/Repositories/Interface/IProductRepository.cs
@@ -1,6 +1,16 @@
 namespace Store.Api.Repositories.Interface;
+using Store.Core.Entities;
 
 public interface IProductRepository
 {
-    
+    //metodo para guardar las categorias del producto
+    Task<Product> SaveAsync(Product product);
+    //metodo para actualizar las categorias del producto
+    Task<Product> UpdateAsync(Product product);
+    //metodo para retornat una lidta de categorias de producto
+    Task<List<Product>> GetAllAsync();
+    //metodo para retornar el id de las categorias del producto que se borrara
+    Task<bool> DeleteAsync(int id);
+    //metodo para obtener una categoria por id
+    Task<Product> GetById(int id);
 }
