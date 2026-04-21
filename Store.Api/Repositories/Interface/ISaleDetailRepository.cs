db1a7e6eaec80cd15eebe02ef2d812a1a03dd429
maribel798-txt <lrs22110150@purisima.tecnm.mx>
2026-04-21 17:44:32 -0600

    crear metodos de salesDetails

--- Store.Api/Repositories/Interface/ISaleDetailRepository.cs
+++ Store.Api/Repositories/Interface/ISaleDetailRepository.cs
@@ -1,6 +1,15 @@
 namespace Store.Api.Repositories.Interface;
-
+using Store.Core.Entities;
 public interface ISaleDetailRepository
 {
-    
+    //metodo para guardar las categorias del producto
+    Task<SaleDetail> SaveAsync(SaleDetail saleDetail);
+    //metodo para actualizar las categorias del producto
+    Task<SaleDetail> UpdateAsync(SaleDetail saleDetail);
+    //metodo para retornat una lidta de categorias de producto
+    Task<List<SaleDetail>> GetAllAsync();
+    //metodo para retornar el id de las categorias del producto que se borrara
+    Task<bool> DeleteAsync(int id);
+    //metodo para obtener una categoria por id
+    Task<SaleDetail> GetById(int id);
 }
