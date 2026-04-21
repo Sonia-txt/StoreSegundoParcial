ca041f669e2c3b9422504e7dd1f3228932bd0f30
Sonia-txt <Soniaitzvaz@gmail.com>
2026-04-21 17:57:45 -0600

    Ajuste de propiedades en la entidad Producto

---
+++ Store.Core/Http/Response.cs
@@ -0,0 +1,12 @@
+namespace Store.Core.Http;
+
+public class Response<T> 
+{   
+    public bool Success { get; set; }
+
+    public T? Data { get; set; }
+
+    public string Message { get; set; } = string.Empty;
+
+    public List<string> Errors { get; set; } = new List<string>();
+}
