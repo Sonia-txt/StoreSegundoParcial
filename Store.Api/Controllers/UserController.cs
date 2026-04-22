eb8f0a4e2ad1c1e2b11309120c02f44aca6f4619
maribel798-txt <lrs22110150@purisima.tecnm.mx>
2026-04-21 19:39:27 -0600

    crear controlls de user

--- Store.Api/Controllers/UserController.cs
+++ Store.Api/Controllers/UserController.cs
@@ -1,6 +1,84 @@
+using Microsoft.AspNetCore.Mvc;
+using Store.Api.Repositories.Interface;
+using Store.Core.Entities;
+using Store.Core.Http;
+
 namespace Store.Api.Controllers;
 
-public class UserController
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
 {
-    
+    private readonly IUserRepository _userRepository;
+
+    public UserController(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<Response<List<User>>>> GetAll()
+    {
+        var users = await _userRepository.GetAllAsync();
+        var response = new Response<List<User>>
+        {
+            Data = users
+        };
+
+        return Ok(response);
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<ActionResult<Response<User>>> Get(int id)
+    {
+        var user = await _userRepository.GetById(id);
+        var response = new Response<User>();
+
+        if (user == null)
+        {
+            response.Errors.Add("USER NOT FOUND");
+            return NotFound(response);
+        }
+
+        response.Data = user;
+        return Ok(response);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Response<User>>> Post([FromBody] User user)
+    {
+        var result = await _userRepository.SaveAsync(user);
+        var response = new Response<User>
+        {
+            Data = result
+        };
+
+        return Created($"/api/[controller]/{result.Id}", response);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<Response<User>>> Update([FromBody] User user)
+    {
+        var result = await _userRepository.UpdateAsync(user);
+        var response = new Response<User>
+        {
+            Data = result
+        };
+
+        return Ok(response);
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    public async Task<ActionResult<Response<bool>>> Delete(int id)
+    {
+        var result = await _userRepository.DeleteAsync(id);
+        var response = new Response<bool>
+        {
+            Data = result
+        };
+
+        return Ok(response);
+    }
 }
